081e68d1a462ddceda5da780a716e4662fe868da
KiritoGuugk <35999227+KiritoGuugk@users.noreply.github.com>
2022-12-09 13:14:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/yym68686/ChatGPT-Telegram-Bot.git > /dev/null
+git clone --depth 1 https://github.com/KiritoGuugk/ChatGPT-Telegram-Bot.git > /dev/null
 echo "code downloaded..." >> /home/log 2>&1
 cd ChatGPT-Telegram-Bot
 pip install -r /home/ChatGPT-Telegram-Bot/requirements.txt > /dev/null
